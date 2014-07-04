c9e2102ca757f5e05146b4dc341b6f971a1d05cf
Manish R <Lazerbolt@Manishs-MacBook-Pro.local>
2014-07-05 07:43:37 +0800

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Penguin.h
@@ -0,0 +1,13 @@
+//
+//  Penguin.h
+//  PeevedPenguins
+//
+//  Created by Manish R on 7/4/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Penguin : CCSprite
+
+@end
