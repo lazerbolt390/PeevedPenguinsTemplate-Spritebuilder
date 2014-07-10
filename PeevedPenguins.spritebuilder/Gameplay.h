cb9ad9fe405f2796f492bbb0e459ae491c332c16
Manish R <Lazerbolt@Manishs-MacBook-Pro.local>
2014-07-10 08:13:45 +0800

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Manish R on 7/10/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
