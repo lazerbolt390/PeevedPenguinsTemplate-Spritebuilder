03fcca8748c865b657a86484f7d2d3edacfbc882
Manish R <Lazerbolt@Manishs-MacBook-Pro.local>
2014-07-15 16:45:15 +0800

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -13,6 +13,7 @@
     CCNode *_levelNode;
     CCNode *_catapultArm;
     CCNode *_contentNode;
+    CCNode *_pullbackNode;
 }
 - (void)didLoadFromCCB {
     // tell this scene to accept touches
@@ -21,6 +22,8 @@
     [_levelNode addChild:level];
     // visualize physics bodies & joints
     _physicsNode.debugDraw = TRUE;
+    // nothing shall collide with our invisible nodes
+    _pullbackNode.physicsBody.collisionMask = @[];
 }
 
 // called on every touch in this scene
